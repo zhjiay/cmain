a278ee2fa28a73d340b171ddda9205fb04fda3fc
zhjiay <zhujiayuan@mekaai.com>
2023-03-01 16:06:53 +0800

    new file:   Essentialcpp/chapter1/inlineTest.cpp 	modified:   Essentialcpp/chapter1/nameTest.cpp 	modified:   Essentialcpp/chapter1/nameTest.exe
    Changes not staged for commit:
    	modified:   Essentialcpp/chapter1/inlineTest.cpp

---
+++ Essentialcpp/chapter1/inlineTest.cpp
@@ -0,0 +1,20 @@
+#include<vector>
+#include<string>
+#include<iostream>
+#define maxINT 2^32-1
+using namespace std;
+
+bool isSizeOK(int size){
+    if(size<=0||size>=maxINT){
+        cerr<<"wrong size";
+        return false;
+    }
+    return truel
+}
+
+
+
+int main(){
+
+    return 0;
+}
--- Essentialcpp/chapter1/nameTest.cpp
+++ Essentialcpp/chapter1/nameTest.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 #include<string>
+#include<vector>
 
 using namespace std;
 //std算是内部继承的库，可以直接通过命名空间调用。
 
-
-int main(){
+void test(){
     std::cout<<"input your name: ";
     std::string firstName;
     std::string lastName;
@@ -14,6 +14,41 @@ int main(){
     std::cin>>lastName;
     std::cout<<"hello, Miss"<<lastName<<std::endl;
     std::cout<<firstName+lastName<<" good bye!";
+}
 
+void exitTest(){
+    int a;
+    cin>>a;
+    if(a<0){
+        cerr<<"Nagative Integer is unaccepteable";
+        exit(-1);
+
+    }else{
+        cout<<"a value is "<<a;
+    }
+}
+
+vector<int> vecTest(int size){
+    vector<int> vec(size);
+    if(size==1){
+        vec[0]=1;
+    }else{
+        vec[0]=1;       
+        for(int i=1;i<size;i++){
+            if(i==1){
+                vec[i]=vec[i-1];
+            }else{
+                vec[i]=vec[i-1]+vec[i-2];
+            }
+        }
+    }
+    return vec;
+}
+int main(){
+
+    vector<int> vect=vecTest(10);
+    for(auto v:vect){
+        cout<<v<<" ";
+    }
     return 1;
 }
